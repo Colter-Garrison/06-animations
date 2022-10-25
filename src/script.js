fe4ade50b0bc6288d0c786c93bf1f4a2cec693b1
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-10-24 19:44:42 -0700

    cube is doing circles with Math.sin and Math.cos

--- src/script.js
+++ src/script.js
@@ -39,7 +39,8 @@ const tick = () => {
     const elapsedTime = clock.getElapsedTime()
 
     //Update Objects
-    mesh.rotation.y = elapsedTime
+    mesh.position.y = Math.sin(elapsedTime)
+    mesh.position.x = Math.cos(elapsedTime)
 
     //Render
     renderer.render(scene, camera)
